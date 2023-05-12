570c908a05ed4d6e7e77b12b7853a0d4d22eec94
Samuel <svaldehita@gmail.com>
2023-05-12 09:23:14 +0200

    Calculadora Añadida

---
+++ Calculadora.java
@@ -0,0 +1,27 @@
+
+public class Calculadora {
+
+	private int x;
+	private int y;
+	
+	public Calculadora(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int Suma () {
+		return x + y;
+	}
+	
+	public int Resta () {
+		return x - y;
+	}
+
+	@Override
+	public String toString() {
+		return "Calculadora [x=" + x + ", y=" + y + ", Suma =" + Suma() + ", Resta =" + Resta() + "]";
+	}
+	
+	
+	
+}
