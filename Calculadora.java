3409469bac2c9604d2b3d9215b5c5f432a614f9a
silvifox27 <131966685+silvifox27@users.noreply.github.com>
2023-05-19 09:16:52 +0200

    Cambio Silvia. Update Calculadora.java
    He añadido una excepción para el método dividir() para evitar posibles errores en la ejecución.
    Cuando se intente dividir entre 0 mostrara un mensaje de error y te devolverá 0 , si no devolverá la división normal.

--- Calculadora.java
+++ Calculadora.java
@@ -23,7 +23,13 @@ public class Calculadora {
 	}
 	
 	public int Divide () {
-		return x / y;
+		if (y==0){
+			System.out.println("Error...");
+			System.out.println("No se puede dividir entre 0");
+			return 0;
+		}else{
+			return x / y;
+		}
 	}
 
 	@Override
